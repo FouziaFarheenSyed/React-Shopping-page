2d25020f87c4c8c97a6a7b34db5e9f8f1b074c25
FouziaFarheenSyed <fouziafarheensyed2003@gmail.com>
2024-07-28 20:32:16 +0530

    v1.1

---
+++ shopping/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './App.css'
+import Mainpage from './Shoppingfolder/Pages/Mainpage'
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Mainpage />
+    </div>
+  )
+}
+
+export default App
---
+++ shopping/src/Shoppingfolder/Pages/Mainpage.jsx
@@ -0,0 +1,30 @@
+
+
+import React,{useState} from 'react'
+import Header from '../Components/Header'
+import Banner from '../Components/Banner'
+import Collections from '../Components/Collections'
+import Footer from '../Components/Footer'
+
+import {Gents,Ladies} from '../data'
+import WomenCollection from '../Components/WomenCollection'
+
+const Mainpage = () => {
+
+  const[gentsFashion, setGentsFashion] = useState(Gents)
+  const[ladiesFashion, setLadiesFashion] = useState(Ladies)
+  console.log(Gents,Ladies)
+
+
+  return (
+    <div>
+        <Header />
+        <Banner />
+        <Collections gentsFashion ={gentsFashion} />
+        <WomenCollection ladiesFashion ={ladiesFashion} />
+        <Footer />
+    </div>
+  )
+}
+
+export default Mainpage
